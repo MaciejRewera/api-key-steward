473b07d393c72b6a5640990ec0116fac3877bdc0
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2024-05-27 12:39:14 +0100

    Implement Base62 encoder/decoder
    The encoding is very simple and has limited set of input values
    to only accept positive Long. This is due to the fact that it is
    supposed to be used only to encode/decode CRC32 algorithm output,
    which is guaranteed to be non-negative Long. Besides, the current
    implementation would return same values for a number and its
    additive inverse.
    For the 2 reasons above it was decided to limit the input space.

---
+++ src/main/scala/apikeysteward/generators/Base62.scala
@@ -0,0 +1,49 @@
+package apikeysteward.generators
+
+import apikeysteward.generators.Base62.Base62Error.ProvidedWithNegativeNumberError
+import cats.implicits.catsSyntaxEitherId
+
+import scala.math.pow
+
+object Base62 {
+
+  val CharacterSet: IndexedSeq[Char] = ('0' to '9') ++ ('A' to 'Z') ++ ('a' to 'z')
+  private val base: Int = CharacterSet.length
+
+  def encode(num: Long): Either[Base62Error, Array[Char]] =
+    if (num < 0) ProvidedWithNegativeNumberError(num).asLeft
+    else encodePos(num).asRight
+
+  private def encodePos(num: Long): Array[Char] = {
+    def loop(n: Long, acc: Array[Char] = Array.empty): Array[Char] =
+      if (n > 0) {
+        val idx = (n % base).toInt
+        val newAcc = CharacterSet(idx) +: acc
+        loop(n / base, newAcc)
+      } else
+        acc
+
+    if (num == 0)
+      Array[Char](CharacterSet(0))
+    else
+      loop(num)
+  }
+
+  def decode(chars: Array[Char]): Long =
+    chars.reverseIterator.zipWithIndex.map { case (char, idx) =>
+      val positionInBase62 = CharacterSet.indexOf(char)
+
+      positionInBase62 * pow(base, idx).toLong
+    }.sum
+
+  sealed trait Base62Error {
+    val message: String
+  }
+
+  object Base62Error {
+    case class ProvidedWithNegativeNumberError(number: Long) extends Base62Error {
+      override val message: String = s"Base62 encoder can only encode non-negative numbers, but received: $number"
+    }
+  }
+
+}
