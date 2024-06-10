5bf334fea985a598379249412cbff4fb0c9f4b20
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2024-06-10 13:06:06 +0100

    Add test case to RandomStringGeneratorSpec

--- src/test/scala/apikeysteward/generators/RandomStringGeneratorSpec.scala
+++ src/test/scala/apikeysteward/generators/RandomStringGeneratorSpec.scala
@@ -3,6 +3,7 @@ package apikeysteward.generators
 import apikeysteward.config.ApiKeyConfig
 import apikeysteward.repositories.SecureHashGenerator.Algorithm.SHA3_256
 import cats.effect.testing.scalatest.AsyncIOSpec
+import fs2.Stream
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AsyncWordSpec
 
@@ -20,6 +21,17 @@ class RandomStringGeneratorSpec extends AsyncWordSpec with AsyncIOSpec with Matc
       val generator = new RandomStringGenerator(config)
 
       generator.generate.asserting(_.length shouldBe config.randomSectionLength)
+    }
+
+    "return different Strings on subsequent calls" in {
+      val generator = new RandomStringGenerator(config)
+
+      for {
+        randomStrings <- Stream.repeatEval(generator.generate).take(42).compile.toVector
+
+        _ = randomStrings.foreach(_.length shouldBe config.randomSectionLength)
+        _ = randomStrings.distinct should contain theSameElementsAs randomStrings
+      } yield ()
     }
 
     "throw exception when provided with length equal to zero" in {
