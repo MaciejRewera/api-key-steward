b8e8ce4364cc3e8384d8c119bdf696b5b3cfbdc7
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2024-08-26 16:02:58 +0100

    Remove unused delete method from ApiKeyDataScopesDb

--- src/main/scala/apikeysteward/repositories/db/ApiKeyDataScopesDb.scala
+++ src/main/scala/apikeysteward/repositories/db/ApiKeyDataScopesDb.scala
@@ -20,12 +20,6 @@ class ApiKeyDataScopesDb()(implicit clock: Clock) {
   def getByApiKeyDataId(apiKeyDataId: Long): Stream[doobie.ConnectionIO, ApiKeyDataScopesEntity.Read] =
     Queries.getByApiKeyDataId(apiKeyDataId).stream
 
-  def delete(apiKeyDataId: Long, scopeId: Long): doobie.ConnectionIO[Option[ApiKeyDataScopesEntity.Read]] =
-    for {
-      result <- Queries.getBy(apiKeyDataId, scopeId).option
-      n <- Queries.delete(apiKeyDataId, scopeId).run
-    } yield if (n > 0) result else None
-
   def delete(apiKeyDataId: Long): Stream[doobie.ConnectionIO, ApiKeyDataScopesEntity.Read] = {
     val result: doobie.ConnectionIO[List[ApiKeyDataScopesEntity.Read]] = for {
       res <- getByApiKeyDataId(apiKeyDataId).compile.toList
@@ -62,11 +56,6 @@ class ApiKeyDataScopesDb()(implicit clock: Clock) {
            |FROM api_key_data_scopes
            |WHERE api_key_data_id = $apiKeyDataId AND scope_id = $scopeId
            |""".stripMargin.query[ApiKeyDataScopesEntity.Read]
-
-    def delete(apiKeyDataId: Long, scopeId: Long): doobie.Update0 =
-      sql"""DELETE FROM api_key_data_scopes
-           |WHERE api_key_data_scopes.api_key_data_id = $apiKeyDataId AND api_key_data_scopes.scope_id = $scopeId
-           """.stripMargin.update
 
     def delete(apiKeyDataId: Long): doobie.Update0 =
       sql"""DELETE FROM api_key_data_scopes
