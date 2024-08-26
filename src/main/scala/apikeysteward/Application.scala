5f51cc5b708454222fff6f4c17458367ebe273b4
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2024-08-26 14:01:38 +0100

    Add DB connection closing to Integration Tests and the Application itself

--- src/main/scala/apikeysteward/Application.scala
+++ src/main/scala/apikeysteward/Application.scala
@@ -97,6 +97,8 @@ object Application extends IOApp.Simple with Logging {
         app = buildServerResource(httpApp, config)
 
         _ <- IO.race(licenseService.periodicallyValidateLicense(), app.useForever)
+
+        _ <- IO.blocking(transactor.kernel.close())
       } yield ()
     }
   }
