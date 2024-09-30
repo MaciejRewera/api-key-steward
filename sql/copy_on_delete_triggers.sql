4834d00ae41b95d5791a8bec8a9e3302d4ad0ac7
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2024-09-30 15:45:23 +0100

    Add tenant_deleted table and database triggers
    The purpose of these triggers is to copy deleted Tenants
    to tenant_deleted table just before deletion.
    This way no data is permanently lost.

---
+++ sql/copy_on_delete_triggers.sql
@@ -0,0 +1,70 @@
+CREATE OR REPLACE FUNCTION copy_tenant() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    INSERT INTO tenant_deleted (deleted_at, tenant_id, public_tenant_id, name, created_at, updated_at, deactivated_at)
+    VALUES (now(),
+            OLD.id,
+            OLD.public_tenant_id,
+            OLD.name,
+            OLD.created_at,
+            OLD.updated_at,
+            OLD.deactivated_at);
+
+    RETURN OLD;
+END;
+$BODY$
+    LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION copy_api_key_data() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    INSERT INTO api_key_data_deleted (deleted_at, api_key_data_id, api_key_id, public_key_id, name, description,
+                                      user_id, expires_at, created_at, updated_at)
+    VALUES (now(),
+            OLD.id,
+            OLD.api_key_id,
+            OLD.public_key_id,
+            OLD.name,
+            OLD.description,
+            OLD.user_id,
+            OLD.expires_at,
+            OLD.created_at,
+            OLD.updated_at);
+
+    RETURN OLD;
+END;
+$BODY$
+    LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION copy_api_key_data_scopes() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    INSERT INTO api_key_data_scopes_deleted (deleted_at, api_key_data_id, scope_id, created_at, updated_at)
+    VALUES (now(),
+            OLD.api_key_data_id,
+            OLD.scope_id,
+            OLD.created_at,
+            OLD.updated_at);
+
+    RETURN OLD;
+END;
+$BODY$
+    LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER copy_tenant_on_deletion
+    BEFORE DELETE
+    ON tenant
+    FOR EACH ROW
+EXECUTE PROCEDURE copy_tenant();
+
+CREATE OR REPLACE TRIGGER copy_api_key_data_on_deletion
+    BEFORE DELETE
+    ON api_key_data
+    FOR EACH ROW
+EXECUTE PROCEDURE copy_api_key_data();
+
+CREATE OR REPLACE TRIGGER copy_api_key_data_scopes_on_deletion
+    BEFORE DELETE
+    ON api_key_data_scopes
+    FOR EACH ROW
+EXECUTE PROCEDURE copy_api_key_data_scopes();
