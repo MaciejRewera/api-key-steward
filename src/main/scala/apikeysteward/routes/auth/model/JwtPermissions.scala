7b1e357057d8effa641183bfdb98dfa171870b45
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2024-10-03 14:06:53 +0100

    Change values of JwtPermissions to contain prefix with service name
    This is a practise I have seen being used by Tableau.
    It allows for clear separation of permissions when a service
    is used as a third party, which is also the case for api-key-steward.
    It protects from users or admins being granted access to
    the third party by a coincidence of the same permission values.

--- src/main/scala/apikeysteward/routes/auth/model/JwtPermissions.scala
+++ src/main/scala/apikeysteward/routes/auth/model/JwtPermissions.scala
@@ -4,9 +4,9 @@ import apikeysteward.routes.auth.JwtAuthorizer.Permission
 
 object JwtPermissions {
 
-  val ReadApiKey: Permission = "read:apikey"
-  val WriteApiKey: Permission = "write:apikey"
+  val ReadApiKey: Permission = "steward:read:apikey"
+  val WriteApiKey: Permission = "steward:write:apikey"
 
-  val ReadAdmin: Permission = "read:admin"
-  val WriteAdmin: Permission = "write:admin"
+  val ReadAdmin: Permission = "steward:read:admin"
+  val WriteAdmin: Permission = "steward:write:admin"
 }
