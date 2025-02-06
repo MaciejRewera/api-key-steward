03246b53d81f910853bea2df0d6e59f1d6300202
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2025-02-06 18:54:07 +0000

    Create PermissionRepositoryItSpec with tests for cascade deletion

--- integration-tests/src/test/scala/apikeysteward/repositories/UserRepositoryItSpec.scala
+++ integration-tests/src/test/scala/apikeysteward/repositories/UserRepositoryItSpec.scala
@@ -4,14 +4,9 @@ import apikeysteward.base.testdata.TenantsTestData.publicTenantId_1
 import apikeysteward.base.testdata.UsersTestData.publicUserId_1
 import apikeysteward.model.User.UserId
 import apikeysteward.repositories.SecureHashGenerator.Algorithm
-import apikeysteward.repositories.db.entity.{
-  ApiKeyDataEntity,
-  ApiKeyEntity,
-  ApiKeyTemplatesUsersEntity,
-  ApiKeysPermissionsEntity
-}
+import apikeysteward.repositories.db.entity._
 import apikeysteward.services.UuidGenerator
-import doobie.ConnectionIO
+import cats.effect.IO
 import doobie.implicits._
 
 class UserRepositoryItSpec extends RepositoryItSpecBase {
@@ -42,26 +37,22 @@ class UserRepositoryItSpec extends RepositoryItSpecBase {
     apiKeyRepository
   )(transactor)
 
-  private def getApiKeyTemplatesUsersAssociations(
-      publicUserId: UserId
-  ): ConnectionIO[List[ApiKeyTemplatesUsersEntity.Read]] =
+  private def getApiKeyTemplatesUsersAssociations(publicUserId: UserId): IO[List[ApiKeyTemplatesUsersEntity.Read]] =
     sql"""SELECT *
          |FROM api_key_templates_users
          |JOIN tenant_user ON tenant_user.id = api_key_templates_users.user_id
          |WHERE tenant_user.public_user_id = ${publicUserId.toString}
-         |""".stripMargin.query[ApiKeyTemplatesUsersEntity.Read].stream.compile.toList
+         |""".stripMargin.query[ApiKeyTemplatesUsersEntity.Read].stream.compile.toList.transact(transactor)
 
-  private def getApiKeysPermissionsAssociations(
-      publicUserId: UserId
-  ): ConnectionIO[List[ApiKeysPermissionsEntity.Read]] =
+  private def getApiKeysPermissionsAssociations(publicUserId: UserId): IO[List[ApiKeysPermissionsEntity.Read]] =
     sql"""SELECT *
          |FROM api_keys_permissions
          |JOIN api_key_data ON api_key_data.id = api_keys_permissions.api_key_data_id
          |JOIN tenant_user ON tenant_user.id = api_key_data.user_id
          |WHERE tenant_user.public_user_id = ${publicUserId.toString}
-         |""".stripMargin.query[ApiKeysPermissionsEntity.Read].stream.compile.toList
+         |""".stripMargin.query[ApiKeysPermissionsEntity.Read].stream.compile.toList.transact(transactor)
 
-  private def getApiKeys(publicUserId: UserId): ConnectionIO[List[ApiKeyEntity.Read]] =
+  private def getApiKeys(publicUserId: UserId): IO[List[ApiKeyEntity.Read]] =
     sql"""SELECT
          |  api_key.id,
          |  api_key.tenant_id,
@@ -71,15 +62,15 @@ class UserRepositoryItSpec extends RepositoryItSpecBase {
          |JOIN api_key_data ON api_key_data.api_key_id = api_key.id
          |JOIN tenant_user ON tenant_user.id = api_key_data.user_id
          |WHERE tenant_user.public_user_id = ${publicUserId.toString}
-         |""".stripMargin.query[ApiKeyEntity.Read].stream.compile.toList
+         |""".stripMargin.query[ApiKeyEntity.Read].stream.compile.toList.transact(transactor)
 
-  private def getApiKeyData(publicUserId: UserId): ConnectionIO[List[ApiKeyDataEntity.Read]] =
+  private def getApiKeyData(publicUserId: UserId): IO[List[ApiKeyDataEntity.Read]] =
     sql"""
          |SELECT *
          |FROM api_key_data
          |JOIN tenant_user ON tenant_user.id = api_key_data.user_id
          |WHERE tenant_user.public_user_id = ${publicUserId.toString}
-         |""".stripMargin.query[ApiKeyDataEntity.Read].stream.compile.toList
+         |""".stripMargin.query[ApiKeyDataEntity.Read].stream.compile.toList.transact(transactor)
 
   "UserRepository on delete" should {
 
@@ -87,11 +78,11 @@ class UserRepositoryItSpec extends RepositoryItSpecBase {
       val result = for {
         _ <- insertPrerequisiteDataAll()
 
-        entitiesBeforeDeletion <- getApiKeyTemplatesUsersAssociations(publicUserId_1).transact(transactor)
+        entitiesBeforeDeletion <- getApiKeyTemplatesUsersAssociations(publicUserId_1)
         _ = entitiesBeforeDeletion should not be empty
 
         _ <- repository.delete(publicTenantId_1, publicUserId_1)
-        res <- getApiKeyTemplatesUsersAssociations(publicUserId_1).transact(transactor)
+        res <- getApiKeyTemplatesUsersAssociations(publicUserId_1)
       } yield res
 
       result.asserting(_ shouldBe List.empty[ApiKeysPermissionsEntity.Read])
@@ -101,11 +92,11 @@ class UserRepositoryItSpec extends RepositoryItSpecBase {
       val result = for {
         _ <- insertPrerequisiteDataAll()
 
-        entitiesBeforeDeletion <- getApiKeysPermissionsAssociations(publicUserId_1).transact(transactor)
+        entitiesBeforeDeletion <- getApiKeysPermissionsAssociations(publicUserId_1)
         _ = entitiesBeforeDeletion should not be empty
 
         _ <- repository.delete(publicTenantId_1, publicUserId_1)
-        res <- getApiKeysPermissionsAssociations(publicUserId_1).transact(transactor)
+        res <- getApiKeysPermissionsAssociations(publicUserId_1)
       } yield res
 
       result.asserting(_ shouldBe List.empty[ApiKeysPermissionsEntity.Read])
@@ -115,11 +106,11 @@ class UserRepositoryItSpec extends RepositoryItSpecBase {
       val result = for {
         _ <- insertPrerequisiteDataAll()
 
-        entitiesBeforeDeletion <- getApiKeyData(publicUserId_1).transact(transactor)
+        entitiesBeforeDeletion <- getApiKeyData(publicUserId_1)
         _ = entitiesBeforeDeletion should not be empty
 
         _ <- repository.delete(publicTenantId_1, publicUserId_1)
-        res <- getApiKeyData(publicUserId_1).transact(transactor)
+        res <- getApiKeyData(publicUserId_1)
       } yield res
 
       result.asserting(_ shouldBe List.empty[ApiKeyDataEntity.Read])
@@ -129,11 +120,11 @@ class UserRepositoryItSpec extends RepositoryItSpecBase {
       val result = for {
         _ <- insertPrerequisiteDataAll()
 
-        entitiesBeforeDeletion <- getApiKeys(publicUserId_1).transact(transactor)
+        entitiesBeforeDeletion <- getApiKeys(publicUserId_1)
         _ = entitiesBeforeDeletion should not be empty
 
         _ <- repository.delete(publicTenantId_1, publicUserId_1)
-        res <- getApiKeys(publicUserId_1).transact(transactor)
+        res <- getApiKeys(publicUserId_1)
       } yield res
 
       result.asserting(_ shouldBe List.empty[ApiKeyEntity.Read])
